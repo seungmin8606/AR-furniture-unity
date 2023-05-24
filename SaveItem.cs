379bbf28d365983d50088c3b0ffc6f930e02d50d
Lee SeungMin <91493937+seungmin8606@users.noreply.github.com>
2023-05-24 20:39:39 +0900

    Add files via upload

---
+++ SaveItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SaveItem
+{
+    public GameObject itemPrefab;
+    public string name;
+    public Vector3 position;
+    public Vector3 scale;
+    public Quaternion rotation;
+}
